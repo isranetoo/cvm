4d8c8188ae313d29dd7fee12df82868f5d95ebb4
Israel Antunes Neto <62219211+isranetoo@users.noreply.github.com>
2025-09-23 00:48:28 -0300

    [FEAT] Add project root path handling to multiple scripts and initialize utils package

--- Balancete/cvm_balancete.py
+++ Balancete/cvm_balancete.py
@@ -1,4 +1,8 @@
 
+import sys, os
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+if project_root not in sys.path:
+	sys.path.insert(0, project_root)
 from utils.cvm_utils import baixar_arquivo
 
 # ================================
